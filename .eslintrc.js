8001f5425d55312e3b25922f66d9fb97f00cb828
supalaaak <supalaaak@gmail.com>
2025-03-12 11:50:46 +0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     rules: {
       // Disable specific rules project-wide
       '@typescript-eslint/no-unused-vars': 'off',
-      'react/no-unescaped-entities': 'off'
+      'react/no-unescaped-entities': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
     }
   };
